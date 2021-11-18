fe87d16d1ac1e8d7ab7e1c728ee271a882eff3fc
infamous0192 <infamous0192@gmail.com>
2021-11-19 04:02:17 +0800

    Add hotkeys to control speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     public float MovementSpeed = 1f;
+    public KeyCode RightButton = KeyCode.RightArrow;
+    public KeyCode LeftButton = KeyCode.LeftArrow;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if (Input.GetKey(RightButton)) {
+            MovementSpeed += 0.2f;
+        }
 
+        if (Input.GetKey(LeftButton)) {
+            MovementSpeed -= 0.2f;
+        }
     }
 }
