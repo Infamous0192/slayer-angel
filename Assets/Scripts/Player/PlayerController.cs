ac337f2528cbc73227c3d355d4628eb6d9c10e6a
Muhammad Faturahman Bancin <mufatraba@gmail.com>
2021-12-24 22:30:47 +0700

    revisi UI mode base

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,24 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
-    public Player player;
-    public Animator animator;
-    public Skill skill;
-    private bool isSkill = false;
+  public Player player;
+  public Animator animator;
+  public Skill skill;
+  private bool isSkill = false;
 
-    private void Start() {
+  // ambil button skil
+  public Button btnHeal;
 
+  private void Start() {
+    btnHeal.onClick.AddListener(() => {
+      isSkill = true;
+    });
+  }
+
+  private void Update() {
+    animator.SetBool("IsAttacking", player.IsAttacking);
+
+    if (Input.GetKey(KeyCode.W) && !player.IsActioning) {
+      animator.SetTrigger("AuraCharge");
+      Instantiate(skill);
+      player.IsActioning = true;
     }
-
-    private void Update() {
-        animator.SetBool("IsAttacking", player.IsAttacking);
-
-        if (Input.GetKey(KeyCode.W) && !player.IsActioning) {
-            animator.SetTrigger("AuraCharge");
-            Instantiate(skill);
-            player.IsActioning = true;
-        }
+    // jika tombol skill heal di tekan
+    else if (isSkill && !player.IsActioning) {
+      animator.SetTrigger("AuraCharge");
+      Instantiate(skill);
+      player.IsActioning = true;
+    } else {
+      isSkill = false;
     }
+  }
 }
